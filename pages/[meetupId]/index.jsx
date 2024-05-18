a2d94c5f5ecd8a426c54bcee37cb15539ad09f80
geroarmani <gerasimgrigorow@gmail.com>
2024-05-18 14:00:28 +0300

    Prepare the pages and updating the gitignore file

---
+++ pages/[meetupId]/index.jsx
@@ -0,0 +1,7 @@
+export default function DetailPage(){
+  return (
+    <>
+      <h1>Single meetup page</h1>
+    </>
+  )
+}
---
+++ pages/new-meetup/index.jsx
@@ -0,0 +1,7 @@
+export default function NewMeetup(){
+  return (
+    <>
+      <h1>Add a meetup event</h1>
+    </>
+  )
+}
